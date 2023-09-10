6d58407250f05c50940a9f16c34131a769635b10
stefanus <stefanus.ayudha@gmail.com>
2023-09-11 00:10:11 +0700

    update dependency

--- feature/navigation-ui/build.gradle.kts
+++ feature/navigation-ui/build.gradle.kts
@@ -13,6 +13,15 @@ android {
 }
 
 dependencies {
+    testApi("junit:junit:4.13.2")
+    androidTestApi("androidx.test.ext:junit:1.1.5")
+    androidTestApi("androidx.test.espresso:espresso-core:3.5.1")
+
+    androidTestApi(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestApi("androidx.compose.ui:ui-test-junit4")
+    debugApi("androidx.compose.ui:ui-tooling")
+    debugApi("androidx.compose.ui:ui-test-manifest")
+
     api(project(":lib:core-common"))
     api(project(":lib:core-ui"))
     implementation(project(mapOf("path" to ":feature:navigation-domain")))
--- lib/core-common/build.gradle.kts
+++ lib/core-common/build.gradle.kts
@@ -7,14 +7,14 @@ android {
 }
 
 dependencies {
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testApi("junit:junit:4.13.2")
+    androidTestApi("androidx.test.ext:junit:1.1.5")
+    androidTestApi("androidx.test.espresso:espresso-core:3.5.1")
 
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestApi(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestApi("androidx.compose.ui:ui-test-junit4")
+    debugApi("androidx.compose.ui:ui-tooling")
+    debugApi("androidx.compose.ui:ui-test-manifest")
 
     // CORE
     api("androidx.core:core-ktx:1.12.0")
@@ -29,7 +29,7 @@ dependencies {
     api("androidx.navigation:navigation-compose:2.7.2")
 
     // KOIN
-    api("io.insert-koin:koin-android:3.1.5")
+    api("io.insert-koin:koin-android:3.4.3")
     api("io.insert-koin:koin-compose:1.0.4")
     api ("io.insert-koin:koin-androidx-compose:3.4.6")
 
@@ -41,7 +41,7 @@ dependencies {
 
     // COMPOSE
     api("androidx.activity:activity-compose:1.7.2")
-    /*implementation("androidx.compose.material3:material3:1.2.0-alpha04")*/
+    /*api("androidx.compose.material3:material3:1.2.0-alpha04")*/
     api("androidx.compose.material3:material3:1.2.0-alpha07")
     api("androidx.compose.ui:ui:1.5.1")
     api("androidx.compose.ui:ui-tooling-preview:1.5.1")
